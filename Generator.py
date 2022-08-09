422ea5cfd79fe3645210db3b06c54d140a8d3f2e
Nicola leone <20nleone@conyers.org.uk>
2022-08-09 13:27:25 +0200

    Initial commit

---
+++ Generator.py
@@ -0,0 +1,11 @@
+
+import random
+
+# Open the file in read mode
+with open("ass.txt", "r") as file:
+	allText = file.read()
+	words = list(map(str, allText.split()))
+
+	# print random string
+	print(random.choice(words))
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# random-insult-generator
+If you are feeling fancy, and you do not like to insult someone in a traditional way, then you found the right repository…
